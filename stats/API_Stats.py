9aee6facf7cf9b1191c445d5f0104240fb73e2a6
AlanPalomino <zappataville@gmail.com>
2020-04-20 20:14:08 -0500

    Update

--- stats/API_Stats.py
+++ stats/API_Stats.py
@@ -16,6 +16,7 @@ from pathlib import Path
 from nltk import ngrams, FreqDist
 import operator
 from nltk.tokenize import RegexpTokenizer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 np.set_printoptions(threshold=sys.maxsize)
 # %%
 tablas =[]
@@ -28,7 +29,7 @@ for path in pathlist:
 
 
 # %%
-def YELP_BUS():
+def YELP():
      # _______________ YELP Bussiness_____________________
      ylp = tablas[0]
      param= []
@@ -69,17 +70,48 @@ def YELP_BUS():
      param.append('Tag de Mayor Frecuencia')
      val.append(max(freqDist.items(), key=operator.itemgetter(1))[0])
 
+     # _________________YELP Reviews_________________________
+     yelp_rev = tablas[6]
+
+     # Reviews Encontrados
+     param.append('Reseñas Encontradas')
+     val.append(int(yelp_rev ['id'].count()))
+     # Polaridad Compuesta de Review
+     revs = yelp_rev['text']
+     sid = SentimentIntensityAnalyzer()
+     polarities = [sid.polarity_scores(review) for review in revs]
+     comp = [polarities[i]['compound'] for i in range(len(polarities))]
+
+     param.append('Mayor polaridad compuesta de Reseña')
+     val.append(max(comp))
+
+     param.append('Menor  polaridad compuesta de Reseña')
+     val.append(min(comp))
+
+     param.append('Promedio de  polaridad compuesta de Reseña')
+     val.append(np.mean(comp))
+
+     #Fecha de Creación
+     param.append('Reseña más Reciente')
+     val.append(yelp_rev['time_created'].max())
+
+     param.append('Reseña más Antigua')
+     val.append(yelp_rev['time_created'].min())
+
+
      # Data Frame Exportado
      data = {'Parámetros':  param,'Valor': val}
 
      df = pd.DataFrame (data, columns = ['Parámetros','Valor'])
-     df.to_excel("Stats_Yelp_Businesses.xlsx")
+     print(df)
+     df.to_excel("Stats_Yelp.xlsx")
+     
 
 # %%
 def FRSQ():
-     # _______________ FOURSQUARE_____________________
+     # ______________ FOURSQUARE_photo_____________________
      fs_ph = tablas[1]
-     fs_venues = tablas[2]
+     
      param= []
      val=[]
      # Usuarios  Encontrados
@@ -89,86 +121,93 @@ def FRSQ():
      param.append('Fotos')
      val.append(fs_ph['venue'].count())
 
+     # ______________ FOURSQUARE_venues_____________________
 
+     fs_venues = tablas[2]
+
+     # Lugares Encontrados
+     param.append('Total Lugares Encontrados')
+     val.append(fs_venues['id'].count())
+
+     # Lugar con Mayor frecuencia
+     plc = fs_venues['name']
+     freqDist = FreqDist(plc)
+     param.append('Lugar con Mayor Frecuencia')
+     val.append(max(freqDist.items(), key=operator.itemgetter(1))[0])
+
+     # Ratings de lugar
+     param.append('Mayor Califiación Asignada')
+     val.append(fs_venues['rating'].max())
+
+     param.append('Menor Calificación Asignada')
+     val.append(fs_venues['rating'].min())
+
+     param.append('Promedio de Calificaciones')
+     val.append(fs_venues['rating'].mean())
+
+     # Descripción de Lugares
+     param.append('Lugares que cuentan con Descripción')
+     val.append(fs_venues['description'].dropna().count())
+
+     # Zona de Mayor Frecuencia
+     region = fs_venues['city']
+     freqDist = FreqDist(region)
+     param.append('Zona de Mayor Frecuencia')
+     val.append(max(freqDist.items(), key=operator.itemgetter(1))[0])
+
+     # Conteo de tips de lugar
+     param.append('Mayor Conteo de tips')
+     val.append(fs_venues['tipCount'].max())
+
+     param.append('Menor Conteo de tips')
+     val.append(fs_venues['tipCount'].min())
+
+     param.append('Promedio de Conteo de tips')
+     val.append(fs_venues['tipCount'].mean())
+
+     # Conteo de likes
+     param.append('Mayor Conteo de likes')
+     val.append(fs_venues['likeCount'].max())
+
+     param.append('Menor Conteo de likes')
+     val.append(fs_venues['likeCount'].min())
+
+     param.append('Promedio de likes')
+     val.append(fs_venues['likeCount'].mean())
+
+     # ______________ FOURSQUARE_lists_____________________
+
+     fs_lists = tablas[3]
+
+     # Lugares  Encontrados
+     param.append('Lugares Encontrados')
+     val.append(fs_lists['id'].count())
+
+     # Lugar con Mayor frecuencia
+     freqDist = FreqDist(fs_lists['name'])
+     param.append('Palabra incluida con Mayor Frecuencia')
+     val.append(max(freqDist.items(), key=operator.itemgetter(1))[0])
 
      # Data Frame Exportado
      data = {'Parámetros':  param,'Valor': val}
-     df = pd.DataFrame (data, columns = ['Parámetros','Valor'])
-     #df.to_excel("Stats_Frsq.xlsx")
-     print(df)
+
+     df2 = pd.DataFrame (data, columns = ['Parámetros','Valor'])
+     df2.to_excel("Stats_Frsq.xlsx")
+
 
 
 # %%
-fs_venues = tablas[2]
-param= []
-val=[]
-fs_ph = tablas[1]
 
-# Usuarios  Encontrados
-param.append('Usuarios Encontrados')
-val.append(fs_ph['id'].count())
-
-param.append('Fotos')
-val.append(fs_ph['venue'].count())
-
-# Lugares Encontrados
-
-param.append('Total Lugares Encontrados')
-val.append(fs_venues['id'].count())
-
- # Lugar con Mayor frecuencia
-plc = fs_venues['name']
-freqDist = FreqDist(plc)
-param.append('Lugar con Mayor Frecuencia')
-val.append(max(freqDist.items(), key=operator.itemgetter(1))[0])
-
-# Ratings de lugar
-param.append('Mayor Califiación Asignada')
-val.append(fs_venues['rating'].max())
-
-param.append('Menor Calificación Asignada')
-val.append(fs_venues['rating'].min())
-
-param.append('Promedio de Calificaciones')
-val.append(fs_venues['rating'].mean())
-
-# Descripción de Lugares
-param.append('Lugares que cuentan con Descripción')
-val.append(fs_venues['description'].dropna().count())
-
-# Zona de Mayor Frecuencia
-region = fs_venues['city']
-freqDist = FreqDist(region)
-param.append('Zona de Mayor Frecuencia')
-val.append(max(freqDist.items(), key=operator.itemgetter(1))[0])
-
-# Conteo de tips de lugar
-param.append('Mayor Conteo de tips')
-val.append(fs_venues['tipCount'].max())
-
-param.append('Menor Conteo de tips')
-val.append(fs_venues['tipCount'].min())
-
-param.append('Promedio de Conteo de tips')
-val.append(fs_venues['tipCount'].mean())
-
-# Conteo de likes
-param.append('Mayor Conteo de likes')
-val.append(fs_venues['likeCount'].max())
-
-param.append('Menor Conteo de likes')
-val.append(fs_venues['likeCount'].min())
-
-param.append('Promedio de likes')
-val.append(fs_venues['likeCount'].mean())
 # %%
-
-# Data Frame Exportado
 data = {'Parámetros':  param,'Valor': val}
 
-df = pd.DataFrame (data, columns = ['Parámetros','Valor'])
-df.to_excel("Stats_Frsq.xlsx")
+df3 = pd.DataFrame (data, columns = ['Parámetros','Valor'])
+#df2.to_excel("Stats_Frsq.xlsx")
 
-#df.to_excel("Stats_Yelp_Businesses.xlsx")
+df3
+
+# %%
+YELP()
+
 
 # %%
